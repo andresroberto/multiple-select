8f993d75d83d45df7a3b87ea70395d5ea95f70cf
Andres Rojas <andres.rojas@auto1.com>
2018-08-01 16:53:34 +0200

    Move logic to get the values into separate fn (getValuesFromSelect)

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 const React = require('react');
 
+const getValuesFromSelect = selectElement =>
+  [...selectElement.options]
+    .reduce((values, option) => {
+      if (option.value) {
+        return [...values, value];
+      }
+
+      return values;
+    }, {});
+
 class MultipleSelect extends React.Component {
   handleChange = (event) => {
-    const values = [...event.target.options]
-      .reduce((values, option) => {
-        if (option.value) {
-          return [...values, value];
-        }
-
-        return values;
-      }, {});
-
+    const values = getValuesFromSelect(event.target);
     this.props.onChange(event, values);
   }
   render() {
